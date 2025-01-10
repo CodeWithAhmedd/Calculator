add5596990f83e602e9c44df8876f3f446f2397e
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2025-01-10 10:01:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 function btnClick(val) {
-    let cal = document.getElementById("cal");
-    let lsatChar = cal.value[cal.value.slice (- 1)];
-    
-    if (val === "C") {
-        cal.value = "";
-       
-     }
-    else if (val === "CE") {
-        cal.value = cal.value.slice(0, -1);
-    }
 
-      else if (val === "=") {
-        cal.value = eval(cal.value);
-    }
-    else{
-        cal.value += val;
-    }
+    let cal = document.getElementById("cal");
+    let lastChar = cal.value.slice(-1);
+    
+    if ((lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/")
+        && (val === "+" || val === "-" || val === "*" || val === "/")) {
+        cal.value = cal.value.slice(0, -1)+val;
+        } else if (val === "=") {
+            cal.value = eval(cal.value);
+        }
+        else if (val === "CE") {
+                    cal.value = cal.value.slice(0, -1);
+                }
+                       else if (val === "C"){
+            cal.value = "";
+        } else {
+            cal.value += val;
+            
+}
 }
